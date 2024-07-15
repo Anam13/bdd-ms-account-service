764756a90068587df79e9298dc33e99083a384f6
Anamika <anamika.g1303@gmail.com>
2024-07-15 22:46:13 +0530

    updated

--- src/main/java/com/account/service/AuthRequestFilter.java
+++ src/main/java/com/account/service/AuthRequestFilter.java
@@ -39,27 +39,30 @@ public class AuthRequestFilter extends OncePerRequestFilter {
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 			throws ServletException, IOException {
 		logger.info("request URI ::" + request.getRequestURI());
-		String username = null;
-		String jwtToken = authTokenUtil.extractTokenFromRequest(request);
-		try {
-			if (!tokenBlacklist.isBlacklisted(jwtToken))
-				username = authTokenUtil.extractUsername(jwtToken);
-			else
-				throw new ExpiredJwtException(null, null, jwtToken);
-		} catch (IllegalArgumentException e) {
-			logger.error("Unable to get JWT Token");
-		} catch (ExpiredJwtException e) {
-			logger.error("JWT Token has expired");
-		}
+		if (SecurityContextHolder.getContext().getAuthentication() == null) {
+			String username = null;
+			String jwtToken = authTokenUtil.extractTokenFromRequest(request);
+			try {
+				if (!tokenBlacklist.isBlacklisted(jwtToken)) {
+					username = authTokenUtil.extractUsername(jwtToken);
+					logger.info("username" + username);
+				} else
+					throw new ExpiredJwtException(null, null, jwtToken);
+			} catch (IllegalArgumentException e) {
+				logger.error("Unable to get JWT Token");
+			} catch (ExpiredJwtException e) {
+				logger.error("JWT Token has expired");
+			}
 
-		if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-			UserDetails userDetails = this.userDetailsServiceImpl.loadUserByUsername(username);
-			if (authTokenUtil.validateToken(jwtToken, userDetails)) {
-				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-						userDetails, null, userDetails.getAuthorities());
-				usernamePasswordAuthenticationToken
-						.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+			if (username != null) {
+				UserDetails userDetails = this.userDetailsServiceImpl.loadUserByUsername(username);
+				if (authTokenUtil.validateToken(jwtToken, userDetails)) {
+					UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+							userDetails, null, userDetails.getAuthorities());
+					usernamePasswordAuthenticationToken
+							.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+					SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+				}
 			}
 		}
 		chain.doFilter(request, response);
